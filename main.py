dea9d0e8a3da7c984c15baae7d1771997a5668db
Jerome Leclanche <jerome@leclan.ch>
2015-12-09 10:28:26 +0200

    Improve caching API and write to the current directory

--- main.py
+++ main.py
@@ -23,22 +23,20 @@ class NGDPCache:
 	HOME = os.path.expanduser("~")
 	XDG_CACHE_HOME = os.environ.get("XDG_CACHE_HOME", os.path.join(HOME, ".cache"))
 
-	def __init__(self, domain):
+	def __init__(self, domain, basedir=XDG_CACHE_HOME):
 		self.domain = domain
-		self.basedir = os.path.join(self.XDG_CACHE_HOME, domain)
-		if not os.path.exists(self.basedir):
-			os.makedirs(self.basedir)
+		self.basedir = os.path.join(basedir, domain)
 
-	def __contains__(self, path):
-		path = os.path.join(self.basedir, path.strip("/"))
+	def contains(self, key, hash):
+		path = os.path.join(self.basedir, key, hash)
 		return os.path.exists(path)
 
-	def get(self, path):
-		path = os.path.join(self.basedir, path.strip("/"))
+	def get(self, key, hash):
+		path = os.path.join(self.basedir, key, hash)
 		with open(path, "rb") as f:
 			return f.read()
 
-	def write(self, key, data, hash):
+	def write(self, key, hash, data):
 		debug("write_to_cache(key=%r, data=%r, hash=%r", key, len(data), hash)
 		dirname = os.path.join(self.basedir, key)
 		if not os.path.exists(dirname):
@@ -48,12 +46,6 @@ class NGDPCache:
 		with open(fname, "wb") as f:
 			f.write(data)
 		log("Written %i bytes to %r" % (len(data), fname))
-
-
-def split_key_and_hash(path):
-	key, hash = os.path.split(path)
-	key = key.strip("/")
-	return key, hash
 
 
 class NGDPConnection:
@@ -105,23 +97,25 @@ class NGDPConnection:
 	def _get_cached_csv(self, path):
 		if path not in self._obj_cache:
 			r = self.get(path)
-			self.cache.write("cdns", r.content, md5(r.content).hexdigest())
+			self.cache.write("cdns", md5(r.content).hexdigest(), r.content)
 			reader = csv.reader(StringIO(r.text), delimiter="|")
 			self._obj_cache[path] = self._parse_csv(reader)
 		return self._obj_cache[path]
 
 	def get_config(self, hash):
-		data = self.cdn_get("/config/{0}/{1}/{2}".format(hash[0:2], hash[2:4], hash))
+		key = "config"
+		if not self.cache.contains(key, hash):
+			data = self.cdn_get("/config/{0}/{1}/{2}".format(hash[0:2], hash[2:4], hash))
+			self.cache.write(key, hash, data)
+		else:
+			data = self.cache.get(key, hash)
 		config = RawConfigParser()
 		config.readfp(StringIO("[DEFAULT]" + data.decode("utf-8")))
 		return dict(config["DEFAULT"])
 
 	def cdn_get(self, path):
-		if path not in self.cache:
-			r = requests.get(self.cdn + path)
-			key, hash = split_key_and_hash(path)
-			self.cache.write(key, r.content, hash)
-		return self.cache.get(path)
+		r = requests.get(self.cdn + path)
+		return r.content
 
 	def get(self, path):
 		return requests.get(self.host + path)
@@ -131,8 +125,12 @@ def main():
 	url = "http://{region}.patch.battle.net:1119/hsb"
 	region = "eu"
 	conn = NGDPConnection(url, region)
+	conn.cache.basedir = "./cache"
 
 	for v in conn.versions:
+		build = v["BuildId"]
+		build_name = v["VersionsName"]
+		print("Found build %s (%r)" % (build_name, build))
 		print(v)
 
 
